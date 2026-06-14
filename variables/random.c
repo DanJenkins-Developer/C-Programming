1a0cf02587ae2acf94a321920e447aae6ffe2ef0
Dan Jenkins <djenkinsjg0617@gmail.com>
2026-06-14 11:40:04 -0500

    Shitty dynamic width

--- variables/random.c
+++ variables/random.c
@@ -1,16 +1,20 @@
 #include <limits.h>
 #include <stdio.h>
 
+void psInt(void);
+
 int main ()
 {
 
-    printf("%-15s %-15s %-15s %-15s %-15s\n", "", "", "Integers", "", "");
-    printf("\n");
-    printf("%-15s %-15s %-15s %-15s %-15s\n", "", "Hex", "Octal", "Int", "Uint");
-    printf("%-15s %-15x %-15o %-15d %-15u\n", "INT_MAX ::" , INT_MAX, INT_MAX, INT_MAX);
-    printf("%-15s %-15x %-15o %-15d %-15u\n", "UINT_MAX ::", UINT_MAX, UINT_MAX, UINT_MAX);
+    int cwidth = 15;
+
+    printf("%015s %015s %-15s %-15s %-15s\n", "Helloooooooooooo", "", "Integers", "", "");
+    printf("%0*s %0*s %-*s %-*s %-*s\n", cwidth, "Hellooooooooo", cwidth, "", cwidth, "Integers", cwidth, "", cwidth, "");
 
     printf("\n");
+
+    // psInt();
+    // printf("\n");
 
     // unsigned unsignedinteger = 0xffffffff;
     // // unsigned unsignedinteger = -0xffffffff;
@@ -22,4 +26,13 @@ int main ()
 
 
     return 0;
+}
+
+void psInt()
+{
+    printf("%-15s %-15s %-15s %-15s %-15s\n", "", "", "Integers", "", "");
+    printf("\n");
+    printf("%-15s %-15s %-15s %-15s %-15s\n", "", "Hex", "Octal", "Int", "Uint");
+    printf("%-15s %-15x %-15o %-15d %-15u\n", "INT_MAX ::" , INT_MAX, INT_MAX, INT_MAX);
+    printf("%-15s %-15x %-15o %-15d %-15u\n", "UINT_MAX ::", UINT_MAX, UINT_MAX, UINT_MAX);
 }
